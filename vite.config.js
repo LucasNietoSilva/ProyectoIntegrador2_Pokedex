adf0466113270ff360d5a4bfdf9a45bdf0c4b633
LucasNietoSilva <nietolucas.lns@gmail.com>
2023-10-25 02:16:36 -0300

    base updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://LucasNietoSilva.github.io/ProyectoIntegrador2_Pokedex"
+  base: "https://github.com/LucasNietoSilva/ProyectoIntegrador2_Pokedex"
 })
